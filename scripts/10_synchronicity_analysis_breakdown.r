0d4aa703c54e6bbe62a237ad5413a0d784b25821
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2026-03-04 16:40:10 -0800

    update

--- scripts/10_synchronicity_analysis_breakdown.r
+++ scripts/10_synchronicity_analysis_breakdown.r
@@ -28,7 +28,7 @@ load(paste0(output_path, "/5_synchronicity_analysis/water_matrices.rda"))
 load(paste0(output_path, "/5_synchronicity_analysis/combined_matrices.rda"))
 
 ###################################################################################################
-########################## Export feed_matrices by date ###########################################
+############## Export feed_matrices by animal, bin, and feed info #################################
 ###################################################################################################
 # Export each dataframe in feed_matrices list as a separate .rda file
 for (date_name in names(feed_matrices)) {
@@ -40,7 +40,7 @@ for (date_name in names(feed_matrices)) {
 }
 
 ###################################################################################################
-########################## Export water_matrices by date ##########################################
+################## Export water_matrices by animal, bin info ######################################
 ###################################################################################################
 # Export each dataframe in water_matrices list as a separate .rda file
 for (date_name in names(water_matrices)) {
@@ -52,7 +52,7 @@ for (date_name in names(water_matrices)) {
 }
 
 ###################################################################################################
-####################### Export combined_matrices by date #########################################
+####################### Export combined_matrices by animal, bin info ##############################
 ###################################################################################################
 # Export each dataframe in combined_matrices list as a separate .rda file
 for (date_name in names(combined_matrices)) {
@@ -62,3 +62,41 @@ for (date_name in names(combined_matrices)) {
   file_path <- paste0(output_path, "/10_synchronicity_analysis_breakdown/combined_matrices/", file_name)
   save(df, file = file_path)
 }
+
+###################################################################################################
+############## Export by date: load *_synch_master_*2.rda and split each day #####################
+###################################################################################################
+# Helper: load rda, create folder, export each dataframe (day) as separate file
+export_by_date_from_rda <- function(rda_path, out_folder, file_prefix) {
+  if (!file.exists(rda_path)) return(invisible(NULL))
+  load(rda_path)
+  out_dir <- paste0(base_dir, "/", out_folder)
+  if (!dir.exists(out_dir)) dir.create(out_dir, recursive = TRUE)
+  for (date_name in names(df)) {
+    date_char <- as.character(date_name)
+    day_df <- df[[date_name]]
+    file_name <- paste0(file_prefix, "_", date_char, ".rda")
+    save(day_df, file = paste0(out_dir, "/", file_name))
+  }
+}
+
+# combined_matrices: animal, bin, feed (feed may not exist for combined)
+export_by_date_from_rda(paste0(base_dir, "/combined_matrices/combined_matrices_synch_master_animal2.rda"),
+  "combined_matrices_animal", "combined_matrices_animal")
+export_by_date_from_rda(paste0(base_dir, "/combined_matrices/combined_matrices_synch_master_bin2.rda"),
+  "combined_matrices_bin", "combined_matrices_bin")
+
+# feed_matrices: animal, bin, feed
+export_by_date_from_rda(paste0(base_dir, "/feed_matrices/feed_matrices_synch_master_animal2.rda"),
+  "feed_matrices_animal", "feed_matrices_animal")
+export_by_date_from_rda(paste0(base_dir, "/feed_matrices/feed_matrices_synch_master_bin2.rda"),
+  "feed_matrices_bin", "feed_matrices_bin")
+export_by_date_from_rda(paste0(base_dir, "/feed_matrices/feed_matrices_synch_master_feed2.rda"),
+  "feed_matrices_feed", "feed_matrices_feed")
+
+# water_matrices: animal, bin, feed (feed may not exist for water)
+export_by_date_from_rda(paste0(base_dir, "/water_matrices/water_matrices_synch_master_animal2.rda"),
+  "water_matrices_animal", "water_matrices_animal")
+export_by_date_from_rda(paste0(base_dir, "/water_matrices/water_matrices_synch_master_bin2.rda"),
+  "water_matrices_bin", "water_matrices_bin")
+
