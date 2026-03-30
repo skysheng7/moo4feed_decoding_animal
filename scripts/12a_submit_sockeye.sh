74cca61ccd7f8c533cfd6bdd7231a740b11a4ae3
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2026-03-30 15:59:24 -0700

    update script

--- scripts/12a_submit_sockeye.sh
+++ scripts/12a_submit_sockeye.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #SBATCH --job-name=pred_models
+#SBATCH --account=st-nina-1
 #SBATCH --array=1-16
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=24:00:00
 #SBATCH --output=logs/12a_model_%a_%A.out
 #SBATCH --error=logs/12a_model_%a_%A.err
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=YOUR_EMAIL@ubc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=skysheng@mail.ubc.ca
 
-# ---- Activate your conda environment ----
-# Adjust the path/name below to match your setup on Sockeye
-source activate moo4feed
-
+cd $SLURM_SUBMIT_DIR
 mkdir -p logs
 
-cd "$SLURM_SUBMIT_DIR"
+module load miniconda3
+conda activate moo4feed
 
 Rscript scripts/12a_predictability_models_hpc.r "$SLURM_ARRAY_TASK_ID"
