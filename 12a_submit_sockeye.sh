8f371cd780bc58f21ed6e2bd2c15ace8f7ca5094
skysheng7 <82303914+skysheng7@users.noreply.github.com>
2026-03-30 17:16:11 -0700

    update

--- 12a_submit_sockeye.sh
+++ 12a_submit_sockeye.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --time=18:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=logs/12a_model_%a_%A.out
 #SBATCH --error=logs/12a_model_%a_%A.err
 #SBATCH --mail-type=ALL
@@ -18,4 +18,4 @@ mkdir -p logs
 module load miniconda3
 source activate moo4feed
 
-Rscript scripts/12a_predictability_models_hpc.r "$SLURM_ARRAY_TASK_ID"
+Rscript --no-init-file scripts/12a_predictability_models_hpc.r "$SLURM_ARRAY_TASK_ID"
